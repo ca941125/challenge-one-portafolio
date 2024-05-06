e86b0fa5a05e8a1c10378ba9a47a4979f1b72262
carolinaAguirre <carolinaaguirre12@gmail.com>
2024-05-05 22:26:33 -0300

    menu hamburguesa

---
+++ index.js
@@ -0,0 +1,12 @@
+//menu hamburguesa responsive 
+document.getElementById("menu-desplegable").addEventListener("click", function() {
+    let dropdownMenu = document.getElementById("dropdown-menu");
+    if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+    } else {
+        dropdownMenu.style.display = "block";
+    }
+});
+
+
+//Haz tú validación en javascript acá
